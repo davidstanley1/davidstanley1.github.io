da44ca1c22a64138caac14dd18d37dfe76063bda
davidstanley1 <73188974+davidstanley1@users.noreply.github.com>
2020-10-20 19:12:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/skeletonDancing.jpg') {
-      myImage.setAttribute('src','images/rattled.jpg');
+      myImage.setAttribute('src','rattled.jpg');
     } else {
-      myImage.setAttribute('src','images/skeletonDancing.jpg');
+      myImage.setAttribute('src','skeletonDancing.jpg');
     }
 }
